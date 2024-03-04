133bc6ac54f56bc262ee90196bf02e5fd7f04144
Prennk <email.affanardana@gmail.com>
2024-03-04 13:12:42 +0700

    [update] attention

--- crd/criterion.py
+++ crd/criterion.py
@@ -151,28 +151,16 @@ class Embed(nn.Module):
 
     def forward(self, x):
         x = x.view(x.shape[0], -1)
-        print('view:', x.shape)
 
         x = self.linear(x)
         x = self.l2norm(x)
 
         x = x.unsqueeze(0)
-        print('unsqueeze:', x.shape)
 
-        # x, _ = self.attention_layers1(x, x, x)
-        # x = self.gelu(x)
-        # x = self.norm1(x)
-
-        # x = self.fc(x)
-        # x = self.l2norm(x)
-
-        # x, _ = self.attention_layers2(x, x, x)
-        # x = self.gelu(x)
-        # x = self.norm1(x)
+        x, _ = self.attention_layers1(x, x, x)
+        x, _ = self.attention_layers2(x, x, x)
 
         x = x.squeeze(0)
-        print('squeeze:', x.shape)
-        print()
 
         return x
 
