cffb9171b2c9cc44d2218a646e219a5ca7990022
Prennk <email.affanardana@gmail.com>
2024-03-04 08:23:08 +0700

    [update] attention

--- crd/criterion.py
+++ crd/criterion.py
@@ -119,14 +119,49 @@ class ContrastNormLoss(nn.Module):
 
 
 
+# class Embed(nn.Module):
+#     """Embedding module"""
+#     def __init__(self, dim_in=1024, dim_out=128):
+#         super(Embed, self).__init__()
+#         self.linear = nn.Linear(dim_in, dim_out)
+#         self.l2norm = Normalize(2)
+
+#     def forward(self, x):
+#         x = x.view(x.shape[0], -1)
+#         x = self.linear(x)
+#         x = self.l2norm(x)
+#         return x
+
+import torch.nn.functional as F
 class Embed(nn.Module):
     """Embedding module"""
     def __init__(self, dim_in=1024, dim_out=128):
         super(Embed, self).__init__()
+        num_layers = 4
+        num_heads = 8
+        self.attention_layers = nn.ModuleList([
+            nn.MultiheadAttention(embed_dim=dim_in, num_heads=num_heads)
+            for _ in range(num_layers)
+        ])
+        self.fc_layers = nn.ModuleList([
+            nn.Linear(dim_in, dim_in)
+            for _ in range(num_layers)
+        ])
+        
         self.linear = nn.Linear(dim_in, dim_out)
         self.l2norm = Normalize(2)
 
     def forward(self, x):
+        x = x.unsqueeze(0)
+
+        residual = x
+        for i in range(self.num_layers):
+            x, _ = self.attention_layers[i](x, x, x)
+            x = F.relu(x)
+            x += residual
+            residual = x
+            
+        x = x.squeeze(0)
         x = x.view(x.shape[0], -1)
         x = self.linear(x)
         x = self.l2norm(x)
