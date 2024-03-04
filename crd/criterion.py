ba2126cf442c201de3eac760c536fd616045f205
Prennk <email.affanardana@gmail.com>
2024-03-04 10:57:56 +0700

    [update] attention

--- crd/criterion.py
+++ crd/criterion.py
@@ -146,12 +146,8 @@ class Embed(nn.Module):
         self.l2norm = Normalize(2)
 
     def forward(self, x):
-        print('x:', x.shape)
         x = x.view(x.shape[0], -1)
-        print('x:', x.shape)
         x = x.unsqueeze(0)
-        print('x:', x.shape)
-        print()
 
         residual = x
         x, _ = self.attention_layers1(x, x, x)
@@ -162,7 +158,6 @@ class Embed(nn.Module):
         x += residual
 
         x = x.squeeze(0)
-        # x = x.view(x.shape[0], -1)
         x = self.linear(x)
         x = self.l2norm(x)
         return x
