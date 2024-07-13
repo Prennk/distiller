d349a71fd78f2f33116d7401b55f882015f95a91
Prennk <email.affanardana@gmail.com>
2024-07-13 14:52:39 +0700

    fix bugs

--- crd/criterion.py
+++ crd/criterion.py
@@ -66,7 +66,8 @@ class CRDLoss(nn.Module):
             s_cluster_loss = self.criterion_cluster_s(out_cluster_s)
             t_cluster_loss = self.criterion_cluster_s(out_cluster_t)
 
-            loss = loss = ((s_loss + t_loss) * 0.5) + ((s_cluster_loss + t_cluster_loss) * 0.5)
+            # loss = loss = ((s_loss + t_loss) * 0.5) + ((s_cluster_loss + t_cluster_loss) * 0.5)
+            loss = s_cluster_loss + t_cluster_loss
         else:
             out_s, out_t = self.contrast(f_s, f_t, idx, contrast_idx)
             s_loss = self.criterion_s(out_s)
