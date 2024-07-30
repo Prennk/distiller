5b5227aea9e1a88db838122ea386708292316843
Prennk <email.affanardana@gmail.com>
2024-07-30 07:36:10 +0700

    80

--- crd/memory.py
+++ crd/memory.py
@@ -200,102 +200,23 @@ class ContrastMemoryModified(nn.Module):
         out_v1 = torch.div(out_v1, Z_v1).contiguous()
         out_v2 = torch.div(out_v2, Z_v2).contiguous()
 
-        # update memory
         with torch.no_grad():
             l_pos = torch.index_select(self.memory_v1, 0, y.view(-1))
             l_pos.mul_(momentum)
             l_pos.add_(torch.mul(v1, 1 - momentum))
+
+            teacher_influence = torch.index_select(self.memory_v2, 0, y.view(-1))
+            l_pos.add_(torch.mul(teacher_influence, 80.0))
+
             l_norm = l_pos.pow(2).sum(1, keepdim=True).pow(0.5)
             updated_v1 = l_pos.div(l_norm)
-            l2_penalty_v1 = 1e-4 * self.memory_v1
             self.memory_v1.index_copy_(0, y, updated_v1)
-            self.memory_v1 -= l2_penalty_v1
 
             ab_pos = torch.index_select(self.memory_v2, 0, y.view(-1))
             ab_pos.mul_(momentum)
             ab_pos.add_(torch.mul(v2, 1 - momentum))
             ab_norm = ab_pos.pow(2).sum(1, keepdim=True).pow(0.5)
             updated_v2 = ab_pos.div(ab_norm)
-            l2_penalty_v2 = 1e-4 * self.memory_v2
             self.memory_v2.index_copy_(0, y, updated_v2)
-            self.memory_v2 -= l2_penalty_v2
 
         return out_v1, out_v2
-
-# class ContrastMemoryModified(nn.Module):
-#     """
-#     memory buffer that supplies large amount of negative samples.
-#     """
-#     def __init__(self, inputSize, outputSize, K, T=0.07, momentum=0.5):
-#         super(ContrastMemoryModified, self).__init__()
-#         self.nLem = outputSize
-#         self.unigrams = torch.ones(self.nLem)
-#         self.multinomial = AliasMethod(self.unigrams)
-#         self.multinomial.cuda()
-#         self.K = K
-
-#         self.register_buffer('params', torch.tensor([K, T, -1, -1, momentum]))
-#         stdv = 1. / math.sqrt(inputSize / 3)
-#         self.register_buffer('memory_v1', torch.rand(outputSize, inputSize).mul_(2 * stdv).add_(-stdv))
-#         self.register_buffer('memory_v2', torch.rand(outputSize, inputSize).mul_(2 * stdv).add_(-stdv))
-
-#     def forward(self, v1, v2, y, idx=None):
-#         K = int(self.params[0].item())
-#         T = self.params[1].item()
-#         Z_v1 = self.params[2].item()
-#         Z_v2 = self.params[3].item()
-
-#         momentum = self.params[4].item()
-#         batchSize = v1.size(0)
-#         outputSize = self.memory_v1.size(0)
-#         inputSize = self.memory_v1.size(1)
-
-#         # original score computation
-#         if idx is None:
-#             idx = self.multinomial.draw(batchSize * (self.K + 1)).view(batchSize, -1)
-#             idx.select(1, 0).copy_(y.data)
-#         # sample
-#         weight_v1 = torch.index_select(self.memory_v1, 0, idx.view(-1)).detach()
-#         weight_v1 = weight_v1.view(batchSize, K + 1, inputSize)
-#         out_v2 = torch.bmm(weight_v1, v2.view(batchSize, inputSize, 1))
-#         out_v2 = torch.exp(torch.div(out_v2, T))
-#         # sample
-#         weight_v2 = torch.index_select(self.memory_v2, 0, idx.view(-1)).detach()
-#         weight_v2 = weight_v2.view(batchSize, K + 1, inputSize)
-#         out_v1 = torch.bmm(weight_v2, v1.view(batchSize, inputSize, 1))
-#         out_v1 = torch.exp(torch.div(out_v1, T))
-
-#         # set Z if haven't been set yet
-#         if Z_v1 < 0:
-#             self.params[2] = out_v1.mean() * outputSize
-#             Z_v1 = self.params[2].clone().detach().item()
-#             print("normalization constant Z_v1 is set to {:.1f}".format(Z_v1))
-#         if Z_v2 < 0:
-#             self.params[3] = out_v2.mean() * outputSize
-#             Z_v2 = self.params[3].clone().detach().item()
-#             print("normalization constant Z_v2 is set to {:.1f}".format(Z_v2))
-
-#         # compute out_v1, out_v2
-#         out_v1 = torch.div(out_v1, Z_v1).contiguous()
-#         out_v2 = torch.div(out_v2, Z_v2).contiguous()
-
-#         with torch.no_grad():
-#             l_pos = torch.index_select(self.memory_v1, 0, y.view(-1))
-#             l_pos.mul_(momentum)
-#             l_pos.add_(torch.mul(v1, 1 - momentum))
-
-#             teacher_influence = torch.index_select(self.memory_v2, 0, y.view(-1))
-#             l_pos.add_(torch.mul(teacher_influence, 7.0))
-
-#             l_norm = l_pos.pow(2).sum(1, keepdim=True).pow(0.5)
-#             updated_v1 = l_pos.div(l_norm)
-#             self.memory_v1.index_copy_(0, y, updated_v1)
-
-#             ab_pos = torch.index_select(self.memory_v2, 0, y.view(-1))
-#             ab_pos.mul_(momentum)
-#             ab_pos.add_(torch.mul(v2, 1 - momentum))
-#             ab_norm = ab_pos.pow(2).sum(1, keepdim=True).pow(0.5)
-#             updated_v2 = ab_pos.div(ab_norm)
-#             self.memory_v2.index_copy_(0, y, updated_v2)
-
-#         return out_v1, out_v2
