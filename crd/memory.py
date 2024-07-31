160baf9f629a49f772c470f06b6960920fb19d23
Prennk <email.affanardana@gmail.com>
2024-07-31 10:04:04 +0700

    attention

--- crd/memory.py
+++ crd/memory.py
@@ -8,18 +8,18 @@ class ContrastMemory(nn.Module):
     """
     memory buffer that supplies large amount of negative samples.
     """
-    def __init__(self, inputSize, outputSize, K, T=0.07, momentum=0.5):
+    def __init__(self, rep_dim, n_data, K, T=0.07, momentum=0.5):
         super(ContrastMemory, self).__init__()
-        self.nLem = outputSize
+        self.nLem = n_data
         self.unigrams = torch.ones(self.nLem)
         self.multinomial = AliasMethod(self.unigrams)
         self.multinomial.cuda()
         self.K = K
 
         self.register_buffer('params', torch.tensor([K, T, -1, -1, momentum]))
-        stdv = 1. / math.sqrt(inputSize / 3)
-        self.register_buffer('memory_v1', torch.rand(outputSize, inputSize).mul_(2 * stdv).add_(-stdv))
-        self.register_buffer('memory_v2', torch.rand(outputSize, inputSize).mul_(2 * stdv).add_(-stdv))
+        stdv = 1. / math.sqrt(rep_dim / 3)
+        self.register_buffer('memory_v1', torch.rand(n_data, rep_dim).mul_(2 * stdv).add_(-stdv))
+        self.register_buffer('memory_v2', torch.rand(n_data, rep_dim).mul_(2 * stdv).add_(-stdv))
 
     def forward(self, v1, v2, y, idx=None):
         K = int(self.params[0].item())
@@ -29,8 +29,8 @@ class ContrastMemory(nn.Module):
 
         momentum = self.params[4].item()
         batchSize = v1.size(0)
-        outputSize = self.memory_v1.size(0)
-        inputSize = self.memory_v1.size(1)
+        n_data = self.memory_v1.size(0)
+        rep_dim = self.memory_v1.size(1)
 
         # original score computation
         if idx is None:
@@ -38,22 +38,22 @@ class ContrastMemory(nn.Module):
             idx.select(1, 0).copy_(y.data)
         # sample
         weight_v1 = torch.index_select(self.memory_v1, 0, idx.view(-1)).detach()
-        weight_v1 = weight_v1.view(batchSize, K + 1, inputSize)
-        out_v2 = torch.bmm(weight_v1, v2.view(batchSize, inputSize, 1))
+        weight_v1 = weight_v1.view(batchSize, K + 1, rep_dim)
+        out_v2 = torch.bmm(weight_v1, v2.view(batchSize, rep_dim, 1))
         out_v2 = torch.exp(torch.div(out_v2, T))
         # sample
         weight_v2 = torch.index_select(self.memory_v2, 0, idx.view(-1)).detach()
-        weight_v2 = weight_v2.view(batchSize, K + 1, inputSize)
-        out_v1 = torch.bmm(weight_v2, v1.view(batchSize, inputSize, 1))
+        weight_v2 = weight_v2.view(batchSize, K + 1, rep_dim)
+        out_v1 = torch.bmm(weight_v2, v1.view(batchSize, rep_dim, 1))
         out_v1 = torch.exp(torch.div(out_v1, T))
 
         # set Z if haven't been set yet
         if Z_v1 < 0:
-            self.params[2] = out_v1.mean() * outputSize
+            self.params[2] = out_v1.mean() * n_data
             Z_v1 = self.params[2].clone().detach().item()
             print("normalization constant Z_v1 is set to {:.1f}".format(Z_v1))
         if Z_v2 < 0:
-            self.params[3] = out_v2.mean() * outputSize
+            self.params[3] = out_v2.mean() * n_data
             Z_v2 = self.params[3].clone().detach().item()
             print("normalization constant Z_v2 is set to {:.1f}".format(Z_v2))
 
@@ -147,18 +147,26 @@ class ContrastMemoryModified(nn.Module):
     """
     Memory buffer that supplies a large amount of negative samples.
     """
-    def __init__(self, inputSize, outputSize, K, T=0.07, momentum=0.5):
+    def __init__(self, rep_dim, n_data, K, T=0.07, momentum=0.5):
         super(ContrastMemory, self).__init__()
-        self.nLem = outputSize
+        self.nLem = n_data
         self.unigrams = torch.ones(self.nLem)
         self.multinomial = AliasMethod(self.unigrams)
         self.multinomial.cuda()
         self.K = K
 
         self.register_buffer('params', torch.tensor([K, T, -1, -1, momentum]))
-        stdv = 1. / math.sqrt(inputSize / 3)
-        self.register_buffer('memory_v1', torch.rand(outputSize, inputSize).mul_(2 * stdv).add_(-stdv))
-        self.register_buffer('memory_v2', torch.rand(outputSize, inputSize).mul_(2 * stdv).add_(-stdv))
+        stdv = 1. / math.sqrt(rep_dim / 3)
+        self.memory_v1 = nn.Parameter(torch.randn(n_data, rep_dim).mul_(2 * stdv).add_(stdv))
+        self.memory_v2 = nn.Parameter(torch.randn(n_data, rep_dim).mul_(2 * stdv).add_(stdv))
+
+        self.query_layer_A = nn.Linear(rep_dim, rep_dim)
+        self.key_layer_A = nn.Linear(rep_dim, rep_dim)
+        self.value_layer_A = nn.Linear(rep_dim, rep_dim)
+        self.query_layer_B = nn.Linear(rep_dim, rep_dim)
+        self.key_layer_B = nn.Linear(rep_dim, rep_dim)
+        self.value_layer_B = nn.Linear(rep_dim, rep_dim)
+        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, v1, v2, y, idx=None):
         K = int(self.params[0].item())
@@ -168,46 +176,49 @@ class ContrastMemoryModified(nn.Module):
 
         momentum = self.params[4].item()
         batchSize = v1.size(0)
-        outputSize = self.memory_v1.size(0)
-        inputSize = self.memory_v1.size(1)
+        n_data = self.memory_v1.size(0)
+        rep_dim = self.memory_v1.size(1)
 
         # original score computation
         if idx is None:
             idx = self.multinomial.draw(batchSize * (self.K + 1)).view(batchSize, -1)
             idx.select(1, 0).copy_(y.data)
-        # sample
+        
+        # contrast v2
         weight_v1 = torch.index_select(self.memory_v1, 0, idx.view(-1)).detach()
-        weight_v1 = weight_v1.view(batchSize, K + 1, inputSize)
-        out_v2 = torch.bmm(weight_v1, v2.view(batchSize, inputSize, 1))
+
+        query_v2 = self.query_layer_A(v2)
+        key_v1 = self.key_layer_A(weight_v1)
+        value_v1 = self.key_layer_A(weight_v1)
+        attention_score_v2 = torch.matmul(query_v2, key_v1.transpose(-2, -1) / math.sqrt(query_v2.size(-1)))
+        attention_weight_v2 = self.softmax(attention_score_v2)
+        out_v2 = torch.matmul(attention_weight_v2, value_v1)
         out_v2 = torch.exp(torch.div(out_v2, T))
-        out_v1_a = torch.bmm(weight_v1, v1.view(batchSize, inputSize, 1))
-        out_v1_a = torch.exp(torch.div(out_v1_a, T))
-        # sample
+
+        # contrast v1
         weight_v2 = torch.index_select(self.memory_v2, 0, idx.view(-1)).detach()
-        weight_v2 = weight_v2.view(batchSize, K + 1, inputSize)
-        out_v1 = torch.bmm(weight_v2, v1.view(batchSize, inputSize, 1))
+
+        query_v1 = self.query_layer_B(v1)
+        key_v2 = self.key_layer_B(weight_v2)
+        value_v2 = self.key_layer_B(weight_v2)
+        attention_score_v1 = torch.matmul(query_v1, key_v2.transpose(-2, -1) / math.sqrt(query_v1.size(-1)))
+        attention_weight_v1 = self.softmax(attention_score_v1)
+        out_v1 = torch.matmul(attention_weight_v1, value_v2)
         out_v1 = torch.exp(torch.div(out_v1, T))
-        out_v2_a = torch.bmm(weight_v2, v2.view(batchSize, inputSize, 1))
-        out_v2_a = torch.exp(torch.div(out_v2_a, T))
 
         # set Z if haven't been set yet
         if Z_v1 < 0:
-            self.params[2] = out_v1.mean() * outputSize
+            self.params[2] = out_v1.mean() * n_data
             Z_v1 = self.params[2].clone().detach().item()
             print("normalization constant Z_v1 is set to {:.1f}".format(Z_v1))
         if Z_v2 < 0:
-            self.params[3] = out_v2.mean() * outputSize
+            self.params[3] = out_v2.mean() * n_data
             Z_v2 = self.params[3].clone().detach().item()
             print("normalization constant Z_v2 is set to {:.1f}".format(Z_v2))
 
         # compute out_v1, out_v2
         out_v1 = torch.div(out_v1, Z_v1).contiguous()
         out_v2 = torch.div(out_v2, Z_v2).contiguous()
-        out_v1_a = torch.div(out_v1, Z_v1).contiguous()
-        out_v2_a = torch.div(out_v2, Z_v2).contiguous()
-
-        out_a = out_v1 + out_v1_a
-        out_b = out_v2 + out_v2_a
 
         # update memory
         with torch.no_grad():
@@ -225,4 +236,4 @@ class ContrastMemoryModified(nn.Module):
             updated_v2 = ab_pos.div(ab_norm)
             self.memory_v2.index_copy_(0, y, updated_v2)
 
-        return out_a, out_b
+        return out_v1, out_v2
